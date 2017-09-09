2a6b04821fda663286d9c9a731f64c265e64e4af
김성훈 <gimseonghun@gim-yeongjaeui-MacBook-Air.local>
2017-09-09 18:42:28 +0900

    test

--- 1/insertionsort/A.java
+++ 1/insertionsort/A.java
@@ -35,7 +35,7 @@ class A{
             bw.append((p+1)+" "+a[p]);
             bw.append("\n");
         }
-        bw.flush();
+        bw.flush(); 
     }
     // public static void main(String[] args) throws IOException{
     //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
