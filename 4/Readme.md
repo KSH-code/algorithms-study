be1a20bd7b60659bb809c49faa7e6cc2bc1a491a
KSH-Code <tjdgnsqn133@gmail.com>
2017-09-30 13:45:03 +0900

    2017-09-30

--- 4/Readme.md
+++ 4/Readme.md
@@ -1,3 +1,5 @@
+# 2017-09-30 풀이 목록
+* Divid & Conquer[코드 보기](dividconquer/Main.java)
 # 2017-09-28 풀이 목록
 * Topolagical Sort[코드 보기](topolagicalsort/Main.java)
 * Quick Sort[코드 보기](quicksort/Main.java)
---
+++ 4/dividconquer/Main.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Main
+ */
+public class Main {
+    private static char mTree[][];
+    private static int mN;
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        mN = Integer.parseInt(br.readLine());
+
+        mTree = new char[mN + 1][mN + 1];
+
+        for (int i = 1; i <= mN; i++) {
+            
+            String inputLine = br.readLine();
+            for (int j = 0; j < mN; j++) {
+                mTree[i][j + 1] = inputLine.charAt(j);
+            }
+        }
+        quadTree(1, 1, 1);        
+        bw.flush();
+    }
+
+    private static void quadTree(int row, int col, int count) throws IOException{
+        if(count > mN)
+            return;
+        char check = mTree[row][col];
+        
+        boolean f = true;
+        for(int i = row;i<=row + mN / count - 1;i++){
+            for(int j = col;j<=col + mN / count - 1;j++){
+                if(check != mTree[i][j] && f){
+                    count *= 2;
+                    bw.write("(");
+                    quadTree(row, col, count); // LT
+                    quadTree(row, col + mN / count, count); // RT
+                    quadTree(row + mN / count, col, count); // LB
+                    quadTree(row + mN / count, col + mN / count, count); // RB
+                    bw.write(")");
+                    f = false;
+                    return;
+                }
+            }
+        }
+        if(f){
+            bw.write(check);
+        }
+    }
+}
