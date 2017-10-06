998c1c7613ccbcde3abac1ef1589d9cfa2499a0c
KSH-Code <tjdgnsqn133@gmail.com>
2017-10-06 12:32:56 +0900

    2017-10-06

--- 5/slidingwindow/Main.java
+++ 5/slidingwindow/Main.java
@@ -11,26 +11,36 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int dp[][] = new int[2][3];
-        int temp[][] = new int[2][3];
+
+        String str1[];
+        int a;
+        int b;
+        int c;
+
+        int temp1, temp2, temp3;
         for(int i = 0; i<N; i++){
 
-            String str1[] = br.readLine().split(" ");
-            int a = Integer.parseInt(str1[0]);
-            int b = Integer.parseInt(str1[1]);
-            int c = Integer.parseInt(str1[2]);
-            
+            str1 = br.readLine().split(" ");
+            a = Integer.parseInt(str1[0]);
+            b = Integer.parseInt(str1[1]);
+            c = Integer.parseInt(str1[2]);
 
-            temp[0][0] = max(dp[0][0] + a, dp[0][1] + a);
-            temp[0][1] = max(dp[0][0] + b, max(dp[0][1] + b, dp[0][2] + b));
-            temp[0][2] = max(dp[0][2] + c, dp[0][1] + c);
 
-            dp[0] = temp[0].clone();
+            temp1 = max(dp[0][0] + a, dp[0][1] + a);
+            temp2 = max(dp[0][0] + b, max(dp[0][1] + b, dp[0][2] + b));
+            temp3 = max(dp[0][2] + c, dp[0][1] + c);
 
-            temp[1][0] = min(dp[1][0] + a, dp[1][1] + a);
-            temp[1][1] = min(dp[1][0] + b, min(dp[1][1] + b, dp[1][2] + b));
-            temp[1][2] = min(dp[1][2] + c, dp[1][1] + c);
+            dp[0][0] = temp1;
+            dp[0][1] = temp2;
+            dp[0][2] = temp3;
 
-            dp[1] = temp[1].clone();
+            temp1 = min(dp[1][0] + a, dp[1][1] + a);
+            temp2 = min(dp[1][0] + b, min(dp[1][1] + b, dp[1][2] + b));
+            temp3 = min(dp[1][2] + c, dp[1][1] + c);
+
+            dp[1][0] = temp1;
+            dp[1][1] = temp2;
+            dp[1][2] = temp3;
         }
 
         bw.write(max(dp[0][0], max(dp[0][1], dp[0][2])) + " " + min(dp[1][0], min(dp[1][1], dp[1][2])));
